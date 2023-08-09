b99b25f2ae05ee9731bd35c15a695baa74f25dc7
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2023-08-09 16:37:01 -0300

    Project zero NEXT com CHAKRA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
-import Image from "next/image";
+import { Box } from "@chakra-ui/react";
 import { Inter } from "next/font/google";
+// import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div>Olá Mundo!!!</div>
+      {/* <div>Olá Mundo</div> */}
+      <Box w="100%" h="100%" bg="red">
+        test
+      </Box>
     </>
   );
 }
